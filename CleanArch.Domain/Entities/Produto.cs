303a7b78bcf473e157fbe47da03d5199d60bdeb0
Felipe Melo <felipe.melo@intelectah.com.br>
2018-12-23 00:28:10 -0300

    Add project files.

---
+++ CleanArch.Domain/Entities/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Entities
+{
+    public class Produto
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ CleanArch.Infra.Data/Context/CleanArchContext.cs
@@ -0,0 +1,39 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class CleanArchContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+        public CleanArchContext(DbContextOptions<CleanArchContext> options) : base(options)
+        {
+            //Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*
+             * Este código remove o comportamento Cascade do On Delete de todas
+             * as Entidades do Model
+             */
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
